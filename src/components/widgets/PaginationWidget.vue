0afbddd04dcbc7f715b61695c2c9301285b58f46
Dave Stewart <dave@davestewart.co.uk>
2019-11-15 16:35:11 +0000

    Add PaginationWidget

---
+++ src/components/widgets/PaginationWidget.vue
@@ -0,0 +1,33 @@
+<template>
+  <LinkWidget title="Pagination" :nodes="links" />
+</template>
+
+<script>
+import LinkWidget, { item } from './LinkWidget'
+
+export default {
+  components: {
+    LinkWidget
+  },
+
+  props: {
+    path: Number,
+    pages: Number,
+  },
+
+  computed: {
+    links () {
+      const path = this.path || this.$route.path.replace(/\/\d+/, '')
+      const items = []
+      for (let i = 0; i < this.pages; i++) {
+        const page = i + 1
+        const link = page > 1
+          ? `${path}/${page}`
+          : path
+        items.push(item(`Page ${page}`, link))
+      }
+      return items
+    }
+  }
+}
+</script>
